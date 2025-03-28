9c4cfb0d5b12bed9623999a85d2d26306a29f7fc
Dan5365 <240754@astanait.edu.kz>
2025-03-28 12:11:54 +0500

    docs,fix,feat

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,39 @@
 import java.util.Scanner;
 
+/**
+ * Класс Main выполняет ввод массива и поиск минимального числа.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
+        // Filling the array
         int arr[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = scanner.nextInt();
-            }
+        // Entering array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
         scanner.close();
 
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            if(arr[i] < min) {
+        // Function call and minimum output
+        System.out.println(findMin(n, arr));
+    }
+
+    /**
+     * Функция для поиска минимального элемента в массиве.
+     * @param n   размер массива
+     * @param arr массив чисел
+     * @return минимальный элемент массива
+     */
+    public static int findMin(int n, int[] arr) {
+        int min = arr[0]; // We take the first element as the initial minimum
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < min) {
                 min = arr[i];
             }
         }
-        System.out.println(min);
+        return min;
     }
 }
-// Временная сложность O(n), так как у нас 2 отдельных цикла for, каждый проходит массив 1 раз.
+// The time complexity is O(n), since we have 2 separate "for" loops, each traversing the array 1 time.
